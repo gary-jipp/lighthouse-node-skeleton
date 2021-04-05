7a399b3f92d57a2e7f3c7f9c4ae8a6583640a1bc
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-04-05 14:54:02 -0700

    Comment

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,7 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {    // Note the bug here!
+  }).done((users) => {    // Note the bug here!  Res is not an array
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
