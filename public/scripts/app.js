c10d80576f86e09e626b5a6d38560ff14c803d3b
Gary J <github@accu.ca>
2021-06-28 21:14:44 -0700

    format

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,7 @@ $(() => {
     method: "GET",
     url: "/api/users"
   }).done((users) => {
-    for(user of users) {    // This assumes data is an array.  - broken!
+    for(user of users) {    // This assumes data is an array!
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
