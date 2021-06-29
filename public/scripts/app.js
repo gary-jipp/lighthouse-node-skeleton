2b3ea2e57536d655df8648811960af538431e873
Gary J <github@accu.ca>
2021-06-28 21:16:02 -0700

    format

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,7 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {    // Note the bug here!  Res is not an array
+  }).done((users) => {    // Note the bug here!  Response Data is not an array
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
